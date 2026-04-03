99499b64dbc1f84c44956817904068a775458ea9
Rasmus Svala <rasmus.svala@hotmail.com>
2026-04-03 08:52:02 +0200

    biome autofix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
+import heroImg from "./assets/hero.png"
 import reactLogo from "./assets/react.svg"
 import viteLogo from "./assets/vite.svg"
-import heroImg from "./assets/hero.png"
 import "./App.css"
 
 function App() {
@@ -40,13 +40,13 @@ function App() {
           <p>Your questions, answered</p>
           <ul>
             <li>
-              <a href="https://vite.dev/" target="_blank">
+              <a href="https://vite.dev/" target="_blank" rel="noopener">
                 <img className="logo" src={viteLogo} alt="" />
                 Explore Vite
               </a>
             </li>
             <li>
-              <a href="https://react.dev/" target="_blank">
+              <a href="https://react.dev/" target="_blank" rel="noopener">
                 <img className="button-icon" src={reactLogo} alt="" />
                 Learn more
               </a>
@@ -61,7 +61,11 @@ function App() {
           <p>Join the Vite community</p>
           <ul>
             <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
+              <a
+                href="https://github.com/vitejs/vite"
+                target="_blank"
+                rel="noopener"
+              >
                 <svg
                   className="button-icon"
                   role="presentation"
@@ -73,7 +77,7 @@ function App() {
               </a>
             </li>
             <li>
-              <a href="https://chat.vite.dev/" target="_blank">
+              <a href="https://chat.vite.dev/" target="_blank" rel="noopener">
                 <svg
                   className="button-icon"
                   role="presentation"
@@ -85,7 +89,7 @@ function App() {
               </a>
             </li>
             <li>
-              <a href="https://x.com/vite_js" target="_blank">
+              <a href="https://x.com/vite_js" target="_blank" rel="noopener">
                 <svg
                   className="button-icon"
                   role="presentation"
@@ -97,7 +101,11 @@ function App() {
               </a>
             </li>
             <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
+              <a
+                href="https://bsky.app/profile/vite.dev"
+                target="_blank"
+                rel="noopener"
+              >
                 <svg
                   className="button-icon"
                   role="presentation"
