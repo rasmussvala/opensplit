028dc09639f527886fbe36e1f7569e5ebb2461a7
Rasmus Svala <rasmus.svala@hotmail.com>
2026-04-11 09:02:08 +0200

    fix: update base path for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH ?? "/",
+  base: "/opensplit/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
